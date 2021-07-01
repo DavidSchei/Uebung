35015c3943fb7ec3728db13264218be559ef5caf
DavidSchei <86786843+DavidSchei@users.noreply.github.com>
2021-07-01 16:50:39 +0200

    Add files via upload

---
+++ SortedList/SortedList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			SortedList<string, int> exam = new SortedList<string, int>();
+
+			exam.Add("Tom", 59);
+			exam.Add("Robert", 95);
+			exam.Add("Sophie", 72);
+			exam.Add("Maria", 68);
+			exam.Add("Nick", 84);
+
+			//your code goes here
+			foreach (string dude in exam.Keys)
+            {
+                if (exam[dude] > 70)
+                {
+                    Console.WriteLine($"{dude}: Passed");
+                }
+                else
+                {
+                    Console.WriteLine($"{dude}: Failed");
+                }    
+            }
+          
+        }
+	}
+    }
+
