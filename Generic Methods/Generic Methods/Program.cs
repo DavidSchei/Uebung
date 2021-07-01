c7ccb5a84c390a3374f8af2938ba25526cafdc68
DavidSchei <86786843+DavidSchei@users.noreply.github.com>
2021-07-01 16:13:53 +0200

    Add files via upload

---
+++ Generic Methods/Generic Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Generic_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int intNum = Convert.ToInt32(Console.ReadLine());
+            double doubNum = Convert.ToDouble(Console.ReadLine());
+
+
+            Printer.Print(text);
+            Printer.Print(intNum);
+            Printer.Print(doubNum);
+        }
+    }
+    class Printer
+    {
+        //your code goes here
+        //wenn du mehrere Typen in einer Methode haben willst musst du generics benutzen
+        //<T> most commonly used
+        //um eben alle Sachen auszugeben text, intNum,doubNum, musst du nicht alles einzeln
+        //nennen, sondern als "data" aufrufen
+        public static void Print<T>(T data) 
+        {
+            Console.WriteLine("Showing " +data);
+        }
+    }
+}
+    
+
